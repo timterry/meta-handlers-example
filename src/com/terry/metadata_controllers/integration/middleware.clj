d04009e9e533f8f9060ef430e30351f68e0bf987
Tim Terry <tim.terry@agentsmutual.co.uk>
2014-11-04 23:01:20 +0000

    Refactored to two projects

--- src/com/terry/metadata_controllers/integration/middleware.clj
+++
@@ -1,6 +0,0 @@
-(ns com.terry.metadata-controllers.integration.middleware
-  (:require [com.terry.metadata-controllers.integration.front-controller :as front-controller]))
-
-(defn wrap-front-controller [namespace handler]
-  (fn [request]
-    (front-controller/handle request namespace)))
--- src/com/terry/metadata_controllers/integration/web2.clj
+++
@@ -1,7 +0,0 @@
-(ns com.terry.metadata-controllers.integration.web2)
-
-(defn ^{:get "/test3.html"} test-page3 [request]
-  {:body "hello3" :status 200})
-
-(defn ^{:post #"/.+\.js"} test-page4 [request]
-  {:body "hello regex3" :status 200})
